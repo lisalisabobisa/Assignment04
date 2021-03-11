4ad855e913a5f7d0b066b9d46f56a3cc1c63e48c
lisalisabobisa <lisamabeles@gmail.com>
2021-03-10 20:18:05 -0800

    init

---
+++ js/01-functions-practice.js
@@ -0,0 +1,44 @@
+//STEP 1
+// function halfNumber(numbers) {
+//     let total = numbers / 2;
+//     console.log("Half of " + numbers + " is " + total);
+// }
+// halfNumber(10);
+
+//STEP 2
+// function squareNumber(numbers) {
+//     let total = Math.pow(numbers, 2);
+//     console.log("The result of squaring the number " + numbers + " is " + total);
+// }
+// squareNumber(20);
+
+//STEP 3
+// function percentOf(num1, num2) {
+//     let total = (num1 / num2) * 100;
+//     console.log(num1 + " is " + total + "% of " + num2);
+// }
+// percentOf(15, 30);
+
+//STEP 4
+// function findModulus(num1, num2) {
+//     let total = num1 % num2;
+//     console.log(total + " is the modulus of " + num1 + " and  " + num2);
+// }
+// findModulus(4, 6);
+
+//STEP 5
+
+// function sumList(a, b, c) {
+//     let nums = prompt("Enter 5 numbers seperated by a comma");
+//     let total = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         if(parseInt(nums[i], 10)) {
+//             nums[i] = parseInt(nums[i]);
+//            total += parseInt(nums[i]);     
+//         }
+//      }
+//     console.log(total + " is the total");
+// }
+// sumList();
+
+
---
+++ js/modules/calculator.js
@@ -0,0 +1,27 @@
+// ADD A PRIVATE FUNCTION CALLED CALCULATE
+let calculate = function(){
+    add = x + y;
+    sub = x - y;
+    multi = x * y;
+    divide = x / y;
+}; 
+   
+
+// ADD FOUR PUBLIC FUNCTIONS BELOW
+
+// ADD FUNCTION
+const add = (x, y) => { return x + y; };
+
+// SUBTRACT FUNCTION
+const sub = (x, y) => { return x - y; };
+
+// MULTIPLY FUNCTION
+const multi = (x, y) => { return x * y; };
+
+// DIVIDE FUNCTION
+const divide = (x, y) => { return x / y; };
+
+// EXPORT THE FOUR PUBLIC FUNCTIONS
+export { add, sub, multi, divide };
+
+
