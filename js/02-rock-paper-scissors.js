fd192cd153352c334a5e349a8ef38f82180f645b
lisalisabobisa <lisamabeles@gmail.com>
2021-03-13 18:35:48 -0800

    init

--- js/02-rock-paper-scissors.js
+++ js/02-rock-paper-scissors.js
@@ -1,61 +1,61 @@
 
 
-let game =function playGame() {
-    let playerChoice;
-    let compChoice = Math.random();
+// let game =function playGame() {
+//     let playerChoice;
+//     let compChoice = Math.random();
  
-    playerChoice = prompt("Choose rock, paper, or scissors")  
-    if (compChoice < 0.34) {
-        compChoice = "rock";
-    } else if (compChoice <= 0.67) {
-        compChoice = "paper";
-    } else {
-        compChoice = "scissors";
-    }
- }
-    console.log(compChoice);
+//     playerChoice = prompt("Choose rock, paper, or scissors")  
+//     if (compChoice < 0.34) {
+//         compChoice = "rock";
+//     } else if (compChoice <= 0.67) {
+//         compChoice = "paper";
+//     } else {
+//         compChoice = "scissors";
+//     }
+//  }
+//     console.log(compChoice);
   
-    let compare = function(choice1, choice2) {
-        if (choice1 === choice2) {
-            alert("It's a tie!");
-        } 
+//     let compare = function(choice1, choice2) {
+//         if (choice1 === choice2) {
+//             alert("It's a tie!");
+//         } 
       
-        if (choice1 === "rock") {
-            if (choice2 === "scissors") {
-                return "rock destroys scissors";
-            } else { 
-                return "paper covers rock";
-            }
-        }
-        if (choice1 === "paper") {
-            if (choice2 === "rock") {
-                return "paper covers rock";
-            } else { 
-                return "scissors cut paper";
-            }
-        } 
-        if (choice1 === "scissors") {
-            if (choice2 === "paper") {
-                return "scissors cut paper";
-            } else { 
-                return "rock destroys scissors";
-            }
-        }
-        if (choice1 === "rope") {
-            alert ("Invalid choice");
-        } else {
-            alert ("Choose Rock, Paper, or Scissors");
-        }
-    }
+//         if (choice1 === "rock") {
+//             if (choice2 === "scissors") {
+//                 return "rock destroys scissors";
+//             } else { 
+//                 return "paper covers rock";
+//             }
+//         }
+//         if (choice1 === "paper") {
+//             if (choice2 === "rock") {
+//                 return "paper covers rock";
+//             } else { 
+//                 return "scissors cut paper";
+//             }
+//         } 
+//         if (choice1 === "scissors") {
+//             if (choice2 === "paper") {
+//                 return "scissors cut paper";
+//             } else { 
+//                 return "rock destroys scissors";
+//             }
+//         }
+//         if (choice1 === "rope") {
+//             alert ("Invalid choice");
+//         } else {
+//             alert ("Choose Rock, Paper, or Scissors");
+//         }
+//     }
     
 
-function compare(playerChoice,compChoice) {
-    if ((playerChoice === compChoice) || (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors") || (playerChoice === "rope")) {
-        playGame();
-    } else {
-        console.log(compare(playerChoice,compChoice));
-    }
-}
+// function compare(playerChoice,compChoice) {
+//     if ((playerChoice === compChoice) || (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors") || (playerChoice === "rope")) {
+//         playGame();
+//     } else {
+//         console.log(compare(playerChoice,compChoice));
+//     }
+// }
 
     
     
@@ -63,4 +63,4 @@ function compare(playerChoice,compChoice) {
 
 
 
-playGame();
+// playGame();
--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,27 +1,27 @@
-// IMPORT THE MODULE
-import { add, sub, multi, divide } from './modules/calculator.js';
-// COLLECT NUMBER 1, NUMBER 2, AND OPERATION FROM THE USER
-let x = parseInt(prompt('Enter a number.'));
-let y = parseInt(prompt('Enter a second number.'));
-let total = prompt('Do you want to add, subtract, multiply, or divide?');
-// CHECK TO SEE WHAT OPERATION THEY'RE USING
+// // IMPORT THE MODULE
+// import { add, sub, multi, divide } from './modules/calculator.js';
+// // COLLECT NUMBER 1, NUMBER 2, AND OPERATION FROM THE USER
+// let x = parseInt(prompt('Enter a number.'));
+// let y = parseInt(prompt('Enter a second number.'));
+// let total = prompt('Do you want to add, subtract, multiply, or divide?');
+// // CHECK TO SEE WHAT OPERATION THEY'RE USING
 
 
-    switch (total) {
-        case "add":
-            alert(add(x, y));
-            break;
-        case "subtract":
-            alert(sub(x, y));
-            break;
-        case "multiply":
-            alert(multi(x, y));
-            break;
-        case "divide":
-            alert(divide(x, y));
-            break;
+//     switch (total) {
+//         case "add":
+//             alert(add(x, y));
+//             break;
+//         case "subtract":
+//             alert(sub(x, y));
+//             break;
+//         case "multiply":
+//             alert(multi(x, y));
+//             break;
+//         case "divide":
+//             alert(divide(x, y));
+//             break;
     
-    }
+//     }
 
-// CALL THE APPROPRIATE FUNCTION
+// // CALL THE APPROPRIATE FUNCTION
 
